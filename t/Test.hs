56aef2d1888cb3ca95a4894074f4298eb12779a5
Edward Kmett <ekmett@gmail.com>
2024-03-29 00:10:56 -0400

    test both with and without profiling, new-test and new-build should be test and build these days, those have long since been made the mainline form

--- t/Test.hs
+++ t/Test.hs
@@ -27,8 +27,19 @@ todoTest = do
   case result of
     Left ex -> do
         let msg = show ex
-        assertBool "todo(1)" $ all (flip isInfixOf msg) [
+        putStrLn msg
+        assertBool "todo(1)" $
+           all (flip isInfixOf msg) [ -- --enable-profiling disabled
                "Control.Placeholder.todo: not yet implemented"
              , "test/Test.hs:26:29"
+             ] ||
+           all (flip isInfixOf msg) [ -- --enable-profiling enabled
+               "Control.Placeholder.todo: not yet implemented",
+               "CallStack (from HasCallStack):",
+                  "todo, called at t/Test.hs:26:29 in main:Main",
+               "CallStack (from -prof):",
+                  "Control.Placeholder.todo (src/Control/Placeholder.hs:116:1-66)",
+                  "Main.todoTest (t/Test.hs:(25,1)-(45,36))",
+                  "Main.CAF (<entire-module>)"
              ]
     _ -> assertBool "todo (1)" False
